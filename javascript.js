5408590069154d609deff764c5f61a34161d461c
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2025-08-29 05:21:39 -0400

    added factory functions skeleton

--- javascript.js
+++ javascript.js
@@ -1,1 +1,45 @@
-console.log('I AM HERE!!!');
+function gameBoard() {
+  console.log('this be gameboard');
+
+  const rows = 3;
+  const columns = 3;
+  board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  function getBoard() {
+    return board;
+  };
+
+  function dropMark() {
+    //
+  };
+
+
+  return {getBoard}
+
+
+};
+
+game = gameBoard();
+
+function Cell() {
+
+  let value = 0;
+
+  function addMark() {
+    value = 'x';
+  };
+
+  function getValue() {
+    return value;
+  };
+
+  return {addMark, getValue};
+
+};
